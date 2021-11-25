d3285f10c3a2239a60509316f6a7cd2a67b83044
Esther Kang <eskang87@gmail.com>
2021-11-25 08:52:35 -0800

    commit tree 407bde28da234e873d36e3cf7e248f0e03a5fa35

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function saturdayFun(activity="roller-skate") {
+/* function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
@@ -10,4 +10,25 @@ let wrapAdjective = function(style="*") {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
   }
-}
+} */
+
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
+
+/* let wrapAdjective = function(highlight="*") {
+    function(adjective){
+  return `You are ${highlight}${adjective}${highlight}!`;
+    }
+}; */
+
+
+let wrapAdjective = function(highlight = "*"){
+  return function(adj = "special") {
+    return `You are ${highlight}${adj}${highlight}!`
+  }
+};
